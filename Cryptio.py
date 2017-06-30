f54070d7318118bc43f79addfbe52b22f3f66829
Bloodyline <bloodyline01@gmail.com>
2017-07-01 00:41:12 +0100

    Add files via upload

--- Cryptio.py
+++ Cryptio.py
@@ -14,16 +14,15 @@ def main():
     parser.add_argument("-d", "--decrypt", help="Decrypt mode", action="store_true", default=False)
     parser.add_argument("-f", "--file", help="adding file to crypt", action="append")
     parser.add_argument("-a", "--allfile", help="Get all file present in the current directory", action="store_const", const=get_dirfiles, default=False)
-    parser.add_argument("-ps", "--parsespeed", help="Define the value of the read/write in bytes of the file", default=512000, type=int)
     args = parser.parse_args()
     files = []
     if args.allfile != False:
         files = args.allfile()
     files = [os.path.basename(x) for x in files]
     if args.encrypt:
-        encrypt(files, args.key, args.parsespeed)
+        encrypt(files, args.key)
     elif args.decrypt:
-        decrypt(files, args.key, args.parsespeed)
+        decrypt(files, args.key)
     else:
         print("[-]No mode choosen")
         exit()
@@ -41,15 +40,15 @@ def _key(password):
     key = base64.urlsafe_b64encode(hash.digest())
     return key
 
-def encrypt(files, password, parsespeed):
+def encrypt(files, password):
     print("[+]Starting the encryption protocole")
+    key = _key(password)
+    encryptor = Fernet(key)
     for file in files:
-        key = _key(password)
-        encryptor = Fernet(key)
         print("\n[+]Encrypting", file)
-        binary_crypt(file, encryptor, parsespeed, encr=1)
+        binary_crypt(file, encryptor, encr=1)
 
-def binary_crypt(filename, cipher, parsespeed, encr=1):
+def binary_crypt(filename, cipher, step=512000, encr=1):
     lenght = 0
     chunk = 0
     with open(filename, "rb") as file:
@@ -59,26 +58,28 @@ def binary_crypt(filename, cipher, parsespeed, encr=1):
         file.seek(0)
         while True:
             if encr:
-                data = cipher.encrypt(file.read(parsespeed))
+                data = cipher.encrypt(file.read(step))
                 #data = base64.urlsafe_b64decode(data)
             else:
-                data = cipher.decrypt(file.read(parsespeed))
+                data = cipher.decrypt(file.read(step))
                 #data = base64.urlsafe_b64decode(data)
-            writeb(filename, parsespeed, data, encr=encr)
-            chunk += parsespeed
+            writeb(filename, step, data, encr=encr)
+            chunk += step
             file.seek(chunk)
             if chunk > lenght:
                 break
-            print("\r[*]Writing:", chunk/1000000, "mb", flush=True, end="")
-def decrypt(files, password, parsespeed):
+            print("\r[*]chunk:", chunk/1000000, "mb", flush=True, end="")
+def decrypt(files, password):
     print("[+]Starting the decryption protocole")
+    key = _key(password)
+    decryptor = Fernet(key)
     for file in files:
-        key = _key(password)
-        decryptor = Fernet(key)
         print("\n[+]Decrypting", file)
-        binary_crypt(file, decryptor, parsespeed, encr=0)
-
-def writeb(file, parsespeed, data, encr=1):
+        try:
+            binary_crypt(file, decryptor, encr=0)
+        except:
+            print("\n[-]Error, password may be not correct")
+def writeb(file, step, data, encr=1):
     filename = ""
     if encr:
         filename = file + ".valin"
